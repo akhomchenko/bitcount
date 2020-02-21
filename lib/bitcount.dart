bd86c2dc2c94e276a98bf3dd997008b750faba0e
Oleksii Khomchenko <akhomchenko@gmail.com>
2020-02-21 08:51:22 -0800

    fix visibility for implementation detail

--- lib/bitcount.dart
+++ lib/bitcount.dart
@@ -5,7 +5,7 @@ import 'package:bitcount/src/js.dart' as js;
 //// second or operand is a fallback in case dart2js implementation changes:
 //// JS bitwise operators work only with lower 32 bits
 const _isJs = (1 << 32) == 0 || (1 << 32) == 1;
-const impl = _isJs ? js.bitCount : native.bitCount;
+const _impl = _isJs ? js.bitCount : native.bitCount;
 
 /// Extends [int] with [bitCount] method that returns number of one-bits.
 extension BitCountInt on int {
@@ -13,6 +13,6 @@ extension BitCountInt on int {
   ///
   /// NOTE: in JS is safe to use with up to 53 bits. See [int].
   int bitCount() {
-    return impl(this);
+    return _impl(this);
   }
 }
